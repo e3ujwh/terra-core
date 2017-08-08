2ee6a3a43aabd48fcf121841c7fdbf3f01ce6b87
C_Hayes,Joseph <C_Hayes.Joseph@Cerner.com>
2017-08-08 17:10:42 -0500

    Working snapshot of terra-signature working with Props and resizing

--- packages/terra-signature/src/Signature.jsx
+++ packages/terra-signature/src/Signature.jsx
@@ -4,21 +4,31 @@ import classNames from 'classnames';
 import 'terra-base/lib/baseStyles';
 import './Signature.scss';
 import $ from 'jquery';
-import {paint} from './actions'
 
 const propTypes = {
- /*
- * Content to be displayed as the name
- */
-  name: PropTypes.string,
+
+  /**
+  * Sets the line width.
+  */
+  lineWidth: PropTypes.number,
+
+  /**
+  * Line segments that define signature.
+  */
+  lineSegments: PropTypes.array,
+
+  /**
+   * A callback function to execute when a line segment is drawn. The first parameter is the event, the 
+   * second parameter is all the line segments, and the last parameter is the most recent line segment drawn.
+   */
+  onChange: PropTypes.func,
+
 };
 
 const defaultProps = {
-
-  /**
-   * Lines drawn for signature.
-   */
-
+  lineWidth: 1,
+  lineSegments: undefined,
+  onChange: undefined,
 };
 
 class Signature extends React.Component {
@@ -27,130 +37,154 @@ class Signature extends React.Component {
     super();
 
     this.state = {
-      coordinates: [],
-      canvasInitialized: false,
-      padWidth: 400,
-      signatureLimits: {
-        minX: null,
-        maxX: null,
-        minY: null,
-        maxY: null
-      }
+      lineSegments: []
     }
-
-    this.setParentNode = this.setParentNode.bind(this);
-
   }
 
-  setParentNode(node) {
-    this.parentNode = node;
+  clearSignature() {
+    this.setState( {lineSegments: [], } );
+
+    var canvas = $('canvas.terra-Signature')[0];
+    var context = canvas.getContext('2d');
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
   }
 
   _addLine(x, y, dragging) {
-    var newLine = {
-      x: x,
-      y: y,
-      dragging: dragging
-    };
+    var newSegment;
+    if (dragging) {
+        var lastLineSegment = this.state.lineSegments[this.state.lineSegments.length-1];
+        newSegment = { x1:lastLineSegment.x2, y1:lastLineSegment.y2, x2:x, y2:y };
+    }
+    else {
+      newSegment = { x1:x, y1:y, x2:x, y2:y };
+    }
 
-    this._updateLimits(x, y);
-
-    // Set New Point Coordinates
+    // Set New Line
     this.setState({
-      coordinates: [...this.state.coordinates, newLine],
-    });
-  }
-
-  _updateLimits(x, y) {
-    var minX = this.state.signatureLimits.minX;
-    var maxX = this.state.signatureLimits.maxX;
-    var minY = this.state.signatureLimits.minY;
-    var maxY = this.state.signatureLimits.maxY;
-
-    if (minX == null || x < minX) { minX = x; }
-    if (maxX == null || x > maxX) { maxX = x; }
-    if (minY == null || y < minY) { minY = y; }
-    if (maxY == null || y > maxY) { maxY = y; }
-
-    this.setState({
-      signatureLimits: {
-        minX: minX,
-        maxX: maxX,
-        minY: minY,
-        maxY: maxY
-      },
+      lineSegments: [...this.state.lineSegments, newSegment],
     });
   }
 
   _draw() {
-    var canvas = $('.esig-signature-body canvas#signature')[0];
+    var canvas = $('canvas.terra-Signature')[0];
     var context = canvas.getContext('2d');
 
-    if (this.state.coordinates.length >= 2) {
-      var previous_point = this.state.coordinates[this.state.coordinates.length-2];
-      var last_point = this.state.coordinates[this.state.coordinates.length-1];
+    if (this.state.lineSegments.length > 0) {
+      var lastLineSegment = this.state.lineSegments[this.state.lineSegments.length-1];
 
-      if (last_point.dragging) {
-        context.strokeStyle = '#000000';
-        context.lineJoin = 'miter';
-        context.lineWidth = 1;
+      context.strokeStyle = '#000000';
+      context.lineJoin = 'round';
 
-        context.beginPath();
+      context.beginPath();
+      context.moveTo(lastLineSegment.x1, lastLineSegment.y1);
+      context.lineTo(lastLineSegment.x2, lastLineSegment.y2);
 
-        context.moveTo(previous_point.x, previous_point.y);
-        context.lineTo(last_point.x, last_point.y);
+      context.stroke();
+    }
+  }
 
-        context.stroke();
-      }
+  _drawSignature(lineSegments) {
+    var canvas = $('canvas.terra-Signature')[0];
+    var context = canvas.getContext('2d');
+
+    context.strokeStyle = '#000000';
+    context.lineJoin = 'round';
+
+    // clear canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    var segmentCount = lineSegments.length;
+    for (var i = 0; i < segmentCount; i++) {
+      context.beginPath();
+      context.moveTo(lineSegments[i].x1, lineSegments[i].y1);
+      context.lineTo(lineSegments[i].x2, lineSegments[i].y2);
+
+      context.stroke();
     }
   }
 
   _mouseDown(event) {
   	var canvas = $('canvas.terra-Signature')[0];
 
-  	dispatch(paint(true));
+    this.setState({painting: true,});
 
- //   this._addLine(event.pageX - canvas.offset().left, event.pageY - canvas.offset().top, false);
+    this._addLine(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop, false);
 
     this._draw();
   }
 
   _mouseUp(event) {
-  	dispatch(paint(false));
+    this.setState({painting: false,});
 
- //   lines = this._convertToLines();
+    if (this.props.onChange) {
+      this.props.onChange(event, this.state.lineSegments, this.state.lineSegments[this.state.lineSegments.length - 1]);
+    }
   }
 
   _mouseXY(event) {
-    var canvas = $('.esig-signature-body canvas#signature');
+    var canvas = $('canvas.terra-Signature')[0];
 
- //   if (this.state.painting) {
- //     this._addLine(event.pageX - canvas.offset().left, event.pageY - canvas.offset().top, true);
- //     this._draw();
- //   }
+    if (this.state.painting) {
+      this._addLine(event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop, true);
+      this._draw();
+    }
   }
-  
+
+  componentWillUnmount() {
+    console.log('Component WILL UNMOUNT!')
+
+    window.removeEventListener("resize", this._updateDimensions);
+  }
+
   componentDidMount() {
   	console.log('Component DID MOUNT!')
 
-//  	var canvas = document.getElementsByClassName('terra-Signature')[0];
-    var canvas = $('canvas.terra-Signature')[0];
+    var canvas = $('canvas.terra-Signature');
 
-  	console.log(canvas);
+    console.log(this.parentNode);
 
   	this.parentNode.addEventListener ('mousedown', e => this._mouseDown (e));
   	this.parentNode.addEventListener ('mousemove', e => this._mouseXY (e));
-  	this.parentNode.addEventListener ('mouseup', e => this._mouseUp (e));
+    document.body.addEventListener('mouseup', e => this._mouseUp (e));
 
-	//For mobile
-	this.parentNode.addEventListener ('touchstart', e => this._mouseDown (e));
-  	this.parentNode.addEventListener ('touchmove', e => this._mouseXY (e));
-  	this.parentNode.addEventListener ('touchend', e => this._mouseUp (e));
+	  //For mobile
+	  this.parentNode.addEventListener ('touchstart', e => this._mouseDown (e), false);
+  	this.parentNode.addEventListener ('touchmove', e => this._mouseXY (e), true);
+  	this.parentNode.addEventListener ('touchend', e => this._mouseUp (e), false);
+    document.body.addEventListener("touchcancel", e => this._mouseUp.bind(this), false);
+
+    this.parentNode.width = canvas.width();
+    this.parentNode.height = canvas.height();
+
+    var context = this.parentNode.getContext('2d');
+
+    let {lineWidth} = Object.assign({}, defaultProps, this.props);
+    context.lineWidth = lineWidth;
+
+    if (this.props.lineSegments) {
+      this.state.lineSegments = this.props.lineSegments;
+      this._drawSignature(this.state.lineSegments);
+    }
+
+    window.addEventListener("resize", e => this._updateDimensions (e));
+
+  }
+
+  _updateDimensions(event) {
+    var canvas = $('canvas.terra-Signature');
+
+    console.log(this.parentNode);
+
+    this.parentNode.width = canvas.width();
+    this.parentNode.height = canvas.height();
+
+    this._drawSignature(this.state.lineSegments);
   }
 
   render() {
     return (
-      <canvas className='terra-Signature' ref={this.setParentNode} />
+      <canvas className='terra-Signature' ref={(node)=>{this.parentNode=node;}} />
     );
   }
 
--- packages/terra-signature/src/actionTypes.js
+++
@@ -1,3 +0,0 @@
-export const SAVE = 'SIGNATURE_SAVE';
-export const PAINT = 'SIGNATURE_PAINT';
-export const CLEAR = 'SIGNATURE_CLEAR';
